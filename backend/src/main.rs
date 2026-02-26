4aecfe4218054a4e5d14b5f87a9df169ddaae977
airplayr <gameundwatch@gmail.com>
2026-02-26 16:44:46 +0900

    環境変数のexampleの追加とbind_addressの環境変数化、Cors規則の内訳化

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,27 +8,36 @@ pub use backend::{AppState, Event, MessagePayload, handle_get_events, handle_pos
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
+  // load environment variables
   dotenv().expect("Failed to load .env file");
+  let bind_address = env::var("BIND_ADDRESS").expect("BIND_ADDRESS not found");
   let origin = env::var("ALLOWED_ORIGIN").expect("ALLOWED_ORIGIN not found");
-  println!("Server running at http://localhost:8080");
-
   let state = web::Data::new(AppState {
     events: Mutex::new(Vec::new()),
   });
 
+  println!("Server running at {}", bind_address);
   HttpServer::new(move || {
-    let cors = Cors::default()
-      .allowed_origin(&origin)
-      .allowed_methods(vec!["GET", "POST"])
-      .allowed_headers(vec!["Content-Type"]);
+    let origin = origin.clone();
+    let cors = if origin == "*" {
+      Cors::default()
+        .allow_any_origin()
+        .allowed_methods(vec!["GET", "POST"])
+        .allowed_headers(vec!["Content-Type"])
+    } else {
+      Cors::default()
+        .allowed_origin(&origin)
+        .allowed_methods(vec!["GET", "POST"])
+        .allowed_headers(vec!["Content-Type"])
+    };
+
     App::new()
       .wrap(cors)
       .app_data(state.clone())
       .service(handle_get_events)
       .service(handle_post_events)
   })
-  .bind("127.0.0.1:8080")?
+  .bind(bind_address)?
   .run()
   .await
 }
